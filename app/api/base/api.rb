84cbb70493c47e719b8d0af7c0792e308c38df8c
yurig <yurig@yurig-All-Series.(none)>
2013-08-14 00:26:03 +0300

    change grape api.

--- app/api/base/api.rb
+++ app/api/base/api.rb
@@ -1,12 +1,25 @@
 module Base
   class Api < Grape::API
     prefix 'api'
-    version 'v1', using: :header, vendor: 'base'
+    version 'api', using: :header, vendor: 'base'
     format :json
 
-    resource :test do
-      get do
+    helpers do
+      def current_user
+        env['warden'].user
+      end
 
+      def authenticate!
+        unless current_user
+          error!('401 Unauthorized', 401)
+        end
+      end
+    end
+
+    resource :reports do
+      get do
+        binding.pry
+        current_user.reports
       end
     end
   end
